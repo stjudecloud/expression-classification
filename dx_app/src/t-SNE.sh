12bffd54c3f1ba7cdabec43bc750caf23e1f972f
Andrew Thrasher <adthrasher@gmail.com>
2020-08-10 12:52:33 -0400

    Adding input sample functionality

--- dx_app/src/t-SNE.sh
+++ dx_app/src/t-SNE.sh
@@ -47,6 +47,19 @@ main() {
     echo $ids | xargs -n 100 | sed "s#^#dx download -o $HOME/in/reference_counts/ --no-progress #" > download_all.sh
     parallel --joblog download.log < download_all.sh
 
+    input_ids=""
+    for ((i = 0; i < ${#input_counts[@]}; i++)) 
+    do
+        id=${input_counts[$i]}
+        #echo "id: $id"
+        clip=$(echo $id | jq '.["$dnanexus_link"]' | sed s'/"//g')
+        input_ids="$input_ids $clip"
+    done
+ 
+    mkdir -p $HOME/in/input_counts/
+    echo $input_ids | xargs -n 100 | sed "s#^#dx download -o $HOME/in/input_counts/ --no-progress #" > download_inputs.sh
+    parallel --joblog download.log < download_inputs.sh
+
     echo ""
     echo "  [*] Retrieving covariates for reference data ..."
     covariates_file=$local_data_dir/covariates.txt
@@ -93,6 +106,31 @@ main() {
       fi
     done
 
+    echo "Getting metadata for input samples" 
+    input_json=$(echo $input_ids | xargs python3 /stjude/bin/bulk_describe.py -p $DX_PROJECT_CONTEXT_ID --ids )
+    echo $input_json > input_metadata.json
+
+    in_arg=""
+    echo "Parsing metadata for each input sample"
+    set +m
+    shopt -s lastpipe
+    echo $input_json | jq -c '.[] | {name: .name, sample_name: .properties.sample_name, disease: .properties.sj_diseases, type: .properties.sample_type, library: .properties.library_type, readlength: .properties.read_length, strandedness: .properties.strandedness, pairing: .properties.pairing}' | while read j
+    do
+      sample_name=$(echo $j | jq '.sample_name')
+      disease_code=$(echo $j | jq '.disease')
+      strandedness=$(echo $j | jq '.strandedness')
+      librarytype=$(echo $j | jq '.library')
+      readlength=$(echo $j | jq '.readlength')
+      pairing=$(echo $j | jq '.pairing')
+      disease_code=$sample_name
+      protocol="${strandedness}_${librarytype}_${pairing}_${readlength}"
+      echo -e "${sample_name}\t${protocol}\t${disease_code}" | sed 's/"//g' >> ${covariates_file}
+      in_arg="$in_arg --input-sample $sample_name"
+      echo "in_arg: $in_arg"
+      echo "Adding input sample: $sample_name, $protocol, $disease_code"
+    done
+    echo ${in_arg}
+
     # Fetch gene blacklist
     echo ""
     echo "  [*] Downloading gene blacklist ..."
@@ -112,7 +150,10 @@ main() {
     then
        tissue_arg="--tissue-type \"${tumor_type}\""
     fi
-    docker run -v $local_data_dir:$container_data_dir -v $local_reference_dir:$container_reference_dir -v $local_output_dir:$container_output_dir stjudecloud/interactive-tsne:dx_native_app bash -c "cd $container_output_dir && itsne-main --debug-rscript -b $container_reference_dir/gene.blacklist.tsv -g $container_reference_dir/gencode.v31.annotation.gtf.gz -c $container_data_dir/covariates.txt -o $container_output_dir/${output_name} ${in_arg} $input_sample_arg $container_data_dir/reference_counts/*.txt --save-data ${tissue_arg}" 
+    echo "docker run -v $local_data_dir:$container_data_dir -v $local_reference_dir:$container_reference_dir -v $local_output_dir:$container_output_dir stjudecloud/interactive-tsne:dx_native_app bash -c \"cd $container_output_dir && itsne-main --debug-rscript -b $container_reference_dir/gene.blacklist.tsv -g $container_reference_dir/gencode.v31.annotation.gtf.gz -c $container_data_dir/covariates.txt -o $container_output_dir/${output_name} ${in_arg} $container_data_dir/input_counts/*.txt $container_data_dir/reference_counts/*.txt --save-data ${tissue_arg}\"" 
+
+
+    docker run -v $local_data_dir:$container_data_dir -v $local_reference_dir:$container_reference_dir -v $local_output_dir:$container_output_dir stjudecloud/interactive-tsne:dx_native_app bash -c "cd $container_output_dir && itsne-main --debug-rscript -b $container_reference_dir/gene.blacklist.tsv -g $container_reference_dir/gencode.v31.annotation.gtf.gz -c $container_data_dir/covariates.txt -o $container_output_dir/${output_name} ${in_arg} $container_data_dir/input_counts/*.txt $container_data_dir/reference_counts/*.txt --save-data ${tissue_arg}" 
 
     # Upload output  
     tsne_plot=$(dx upload $local_output_dir/${output_name} --brief)
